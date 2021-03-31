13c4e8a8048fde99ae868dcce208e81e3ee9bf8a
Danilo Dimitrievski <dimitrievski.danilo@gmail.com>
2021-03-31 16:23:17 +0200

    plan routes fully rest

--- src/routes/plan.js
+++ src/routes/plan.js
@@ -15,4 +15,60 @@ router.post("/", (req,res)=> {
     });
 });
 
+router.get("/", (req, res) => {
+    const {query} = req;
+
+    Plan.find({
+        ...query
+    }).then(plans=>{
+        res.json(plans);
+    }).catch(err=>{
+        console.log(err);
+        res.status(500).json({
+            message: err.message
+        })
+    })
+});
+
+router.get("/:id", (req, res)=>{
+    const {id} = req.params;
+
+    Plan.findById(id).then(plan=>{
+        res.json(plan);
+    }).catch(err=>{
+        res.status(500).json({
+            message: err.message
+        });
+    })
+});
+
+router.delete("/:id", (req,res) => {
+    const {id} = req.params;
+
+    Plan.findByIdAndDelete(id).then(()=>{
+        res.json({
+            message: "Successfully deleted"
+        });
+    }).catch(err=>{
+        console.log(err);
+        res.status(404).json({
+            message: err.message
+        });
+    })
+});
+
+router.patch("/:id", (req,res) => {
+    const {body, params} = req;
+    const {id} = params;
+
+    Plan.findByIdAndUpdate(id, {...body}, {new: true}).exec().then(plan=>{
+        res.json(plan);
+    }).catch(err=>{
+        console.log(err.message);
+        res.status(500).json({
+            message: err.message
+        })
+    })
+});
+
 module.exports = router;
